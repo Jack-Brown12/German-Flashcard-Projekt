5aa91ba20062390c2c0e2c34ee1df2cc512498b1
Jack-Brown12 <j84brown@uwaterlo.ca>
2026-01-01 17:01:20 -0500

    Append CORS configuration

--- app/main.py
+++ app/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",   # Vite default
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://german-flashcard-projekt.vercel.app" # Deployment
     ],
     allow_credentials=True,
     allow_methods=["*"],
